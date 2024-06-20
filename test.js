b09dfa22da7b2fea4725772e2bc13ba582b037b7
Georgii Novikov <goshan.0211@yandex.ru>
2024-06-20 23:01:41 +0300

    изменена гамма, добавлены: индекатор загрузки, советы, логотип-кнопка

--- test.js
+++ test.js
@@ -1,18 +1,39 @@
-function waitingFile() {
+/*function waitingFile() {
     let temp = 0;
     while (temp === 0) {
-        const fileInput = document.getElementById("file");
+        const fileInput = document.getElementById("f_file");
         if (fileInput.length === undefined) {
             temp = 1;
         }
     }
-}
+}*/
 
-async function myFunction() {
-    await waitingFile()
+async function uploadFunc() {
+    /*await waitingFile()
 
-    const fileInput = document.getElementById("file");
+    const fileInput = document.getElementById("f_file");
     for (const file of fileInput.files) {
         alert(file.name);
-    }
+    }*/
+
+    const dropZone = document.getElementById('dropZone')
+    const button = document.getElementById("f_button");
+    const upload = document.getElementById("f_upload");
+    const label = document.getElementById("f_label");
+
+    /*upload.textContent = "готово"*/
+    upload.textContent = "обрабатываем файл..."
+
+    button.style.display = 'none';
+    upload.style.display = 'block';
+    label.style.display = 'none';
+
+    const loadBox = document.createElement('div');
+    loadBox.className = "f_load_box";
+
+    const load = document.createElement('div');
+    load.className = "f_load";
+    loadBox.appendChild(load);
+
+    dropZone.appendChild(loadBox);
 }
