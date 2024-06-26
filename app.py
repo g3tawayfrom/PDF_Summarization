2c9ca1f1fc410c30d5f9b680818f0cc35e07a5dd
Viktor <333720@niuitmo.ru>
2024-06-26 15:37:27 +0300

    add styles

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import os
 from flask import Flask, request, render_template, send_file
-from pdf_processing import extract_text, summarize_with_bert, summarize_with_gpt2, summarize_with_xlnet, preprocessing
+from summarization import extract_text, summarize_with_bert, summarize_with_gpt2, summarize_with_xlnet
 from fpdf import FPDF
 
 app = Flask(__name__)
@@ -20,18 +20,22 @@ def summarize():
             text = extract_text(file)
 
     if text.strip():
-        preprocessed_text = preprocessing(text)
-        bert_summary = summarize_with_bert(preprocessed_text)
-        gpt2_summary = summarize_with_gpt2(preprocessed_text)
-        xlnet_summary = summarize_with_xlnet(preprocessed_text)
+        bert_summary = summarize_with_bert(text)
+        gpt2_summary = summarize_with_gpt2(text)
+        xlnet_summary = summarize_with_xlnet(text)
 
-        summary_text = f"BERT Summary:\n{bert_summary}\n\nGPT-2 Summary:\n{gpt2_summary}\n\nXLNet Summary:\n{xlnet_summary}"
+        summary = {
+            "bert": bert_summary,
+            "gpt2": gpt2_summary,
+            "xlnet": xlnet_summary
+        }
+
+        pdf_path = save_summary_to_pdf(summary)
     else:
-        summary_text = "No valid text provided."
+        summary = None
+        pdf_path = None
 
-    pdf_path = save_summary_to_pdf(summary_text)
-
-    return render_template('index.html', summary=summary_text, text=text, pdf_path=pdf_path)
+    return render_template('index.html', summary=summary, text=text, pdf_path=pdf_path)
 
 @app.route('/download_pdf')
 def download_pdf():
@@ -44,11 +48,13 @@ def save_summary_to_pdf(summary):
     pdf = FPDF()
     pdf.add_page()
     pdf.set_font("Arial", size=12)
-    pdf.multi_cell(0, 10, summary)
+    for key, value in summary.items():
+        pdf.multi_cell(0, 10, f"{key.capitalize()} Summary:\n{value}\n\n")
     pdf_path = os.path.join("summaries", "summary.pdf")
     os.makedirs(os.path.dirname(pdf_path), exist_ok=True)
     pdf.output(pdf_path)
     return pdf_path
+
 
 if __name__ == '__main__':
     app.run(debug=True)
